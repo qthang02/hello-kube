94ead594f847f0533f64b4f783054f3dfbdf8838
qthang02 <nguyenquocthang909@gmail.com>
2023-06-30 09:45:47 +0700

    install express

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
-const http = require("http");
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.end("Hello kube\n")
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-server.listen(3000, () => {
-  console.log("Server listen on port 3000")
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
