58aa1615aba81468fbc94c1d674bc036fb1a8b0b
qthang02 <nguyenquocthang909@gmail.com>
2023-07-07 19:37:14 +0700

    this is commit

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/khong-che', (req, res) => {
+  res.send('day la trang khong che');
+});
+
+app.get('/co-che', ( req, res ) => {
+  res.send(' day la trang co che ');
+});
+
+app.get('/javvietsub/:id', ( req, res ) => {
+  const id = req.params.id;
+  res.send(`day la trang jav vietsub ${id}`);
+});
+
+
+app.get('/javKorea/:title', ( req, res ) => {
+  const title = req.params.title;
+  res.send('day la trang jav korea ' + title);
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
